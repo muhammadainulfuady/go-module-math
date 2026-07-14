363b927d4ed9b38d5b858730f46ff41c61ef2ae6
Muhammad Ainul Fuady <ainulfuadi1234@gmail.com>
2026-07-14 17:26:41 +0700

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/muhammadainulfuady/go-module-math
+
+go 1.26.2
---
+++ modMath.go
@@ -0,0 +1,13 @@
+package go_module_math
+
+func Tambah(a int, b int) int {
+	return a + b
+}
+
+func Kali(a int, b int) int {
+	return a * b
+}
+
+func Kurang(a int, b int) int {
+	return a + b
+}
